cecbb4ed6c17a04d4c4ebfb90a8797871e757134
esadakman <esadd26@gmail.com>
2023-01-30 16:37:48 +0300

    webhook created

---
+++ pages/api/auth/webhook.js
@@ -0,0 +1,77 @@
+import { buffer } from "micro";
+import * as admin from "firebase-admin";
+
+// Secure a connection to Firebase from the backend.
+// authenticate the admin. this is backend stuff, the other firebase.js
+// was for frontend work
+// this could also be an import:
+
+const serviceAccount = require("../../../firebasePermissions.json");
+
+const app = !admin.apps.length
+  ? admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+    })
+  : admin.app();
+
+  console.log(JSON.parse(session));
+// Establish a connection to Stripe.
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const endpointSecret = process.env.STRIPE_SIGNING_SECRET;
+
+const fulfillOrder = async (session) => {
+  console.log("Fulfilling order", session);
+  return app
+    .firestore()
+    .collection("users")
+    .doc(session.metadata.email)
+    .collection("orders")
+    .doc(session.id)
+    .set({
+      amount: session.amount_total / 100,
+      amount_shipping: session.total_details.amount_shipping / 100,
+      images: JSON.parse(session.metadata.images),
+      timestamp: admin.firestore.FieldValue.serverTimestamp(),
+    })
+    .then(() => {
+      console.log(`SUCCESS: Order ${session.id} has been added to the DB`);
+    });
+};
+
+export default async (req, res) => {
+  if (req.method === "POST") {
+    const requestBuffer = await buffer(req);
+    const payload = requestBuffer.toString();
+    const secret = req.headers["stripe-signature"];
+
+    let event;
+
+    // Verify that the event came from Stripe
+    try {
+      event = stripe.webhooks.constructEvent(payload, secret, endpointSecret);
+    } catch (err) {
+      console.log(`❌ Error message: ${err.message}`);
+      return res.status(400).send(`Webhook Error: ${err.message}`);
+    }
+
+    // Handle the checkout.session.completed event
+    if (event.type === "checkout.session.completed") {
+      const session = event.data.object;
+
+      // Fulfill the order...
+      return fulfillOrder(session)
+        .then(() => res.status(200))
+        .catch((err) => res.status(400).send(`Webhook Error: ${err.message}`));
+    }
+
+    // Return a response to acknowledge receipt of the event
+    res.json({ received: true });
+  }
+};
+
+export const config = {
+  api: {
+    bodyParser: false,
+    externalResolver: true, // resolved by stripe
+  },
+};
--- pages/checkout.js
+++ pages/checkout.js
@@ -20,7 +20,7 @@ function Checkout() {
     const stripe = await stripePromise;
     // call the backend to create a checkout session...
     const checkoutSession = await axios.post("/api/create-checkout-session", {
-      items,
+      items: items,
       email: session.user.email,
     });
 
